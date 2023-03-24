e45b6488297004e3517fe8ea3888239870a7b1a7
Farid <ferid2805@gmail.com>
2023-03-24 15:01:28 +0200

    Login Functionality

--- src/test/java/com/UPGENIX/runners/cukesRunner.java
+++ src/test/java/com/UPGENIX/runners/cukesRunner.java
@@ -6,8 +6,8 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"html:target/cucumber-reports.html",
-         "json:target/cucumber.json"},
+        plugin = "html:target/cucumber-reports.html",
+
 
         features = "src/test/resources/features",
         glue = "com/UPGENIX/step_definitions",
